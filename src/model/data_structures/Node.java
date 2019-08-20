41d274b644285c75eb7d1c85a41c95ff30d306a2
daniellondonob <ds.londono@uniandes.edu.co>
2019-08-19 20:05:40 -0500

    Implementacion de la LinkedList y de la clase Node. TODO: Asserts, Node.java

---
+++ src/model/data_structures/Node.java
@@ -0,0 +1,34 @@
+private static class Node<Item>
+{
+
+	/**
+	* Atributo que modela el parametro de la clase  
+	*/
+	private Item data;
+
+	/**
+	*	Atributo que modela el Nodo siguiente
+	*/
+	private Node<Item> siguiente;
+
+
+
+
+	///////////////////////////////////
+	////	Constructor		///////////
+	///////////////////////////////////
+	/**
+	*	Constructor de la clase
+	*	@param pData 
+	*	@param pSIguiente Nodo siguiente 
+	*/
+	public Node( Item pData, Node<Item> pSiguiente)
+	{
+		this.data = data;
+
+		this.siguiente = pSiguiente;
+
+	}
+
+
+}
